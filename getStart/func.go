63d3284c0299a64a42fe9b434402cb5bb8efa60f
Liyb2002 <55519796+Liyb2002@users.noreply.github.com>
2021-12-31 19:35:21 -0500

    Update func.go

--- getStart/func.go
+++ getStart/func.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "math"
 
 func eval(a,b int, op string)int{
 	switch op{
@@ -21,9 +22,17 @@ func div(a,b int)(q,r int){
 	q=a/b
 	r=a%b
 	return 
-	
+
 }
 
+func apply(op func(int, int)int, a,b int)int{
+	return op(a,b)
+}
+//函数式编程
 func main(){
 	fmt.Println(eval(3,4, "+"))
+	fmt.Println(apply(
+		func (a int, b int)int {
+			return int(math.Pow(float64(a), float64(b)))
+		}, 3,4 ))
 }
