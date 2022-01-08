fbb11ae5e5af7578a6d9f5e283d37ba049f72968
Liyb2002 <55519796+Liyb2002@users.noreply.github.com>
2022-01-08 12:51:18 -0500

    findall

--- getStart/GoExamples/regex.go
+++ getStart/GoExamples/regex.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 )
 
-const text = "my email is yl4092@Columbia.edu"
+const text = "my email is yl4092@Columbia.edu  yl40492@Columbia.edu  yl2@Columbia.edu"
 
 func main(){
 	re := regexp.MustCompile(`[a-zA-Z0-9]+@.+\..+`)
 
-	match := re.FindString(text)
+	match := re.FindAllString(text, -1)
 	fmt.Println(match)
 }
