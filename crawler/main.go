ba4ba0a1e814559bd7199d0a1173ce13b8f97cb4
Liyb2002 <55519796+Liyb2002@users.noreply.github.com>
2022-01-09 16:08:31 -0500

    cityUrl_arrays

--- crawler/main.go
+++ crawler/main.go
@@ -3,41 +3,25 @@ package main
 import (
 	"fmt"
 	"regexp"
-	//"net/http"
-//	"io/ioutil"
 	"crawler/fetcher"
 )
 
 func main(){
-	//fetcher.TestPrint()
-	/*
-	resp, err := http.Get("http://www.zhenai.com/zhenghun")
-
-	if err !=nil{
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode == http.StatusOK{
-		all, err := ioutil.ReadAll(resp.Body)
-		if err!=nil{
-			panic(err)
-		}
-		fmt.Printf("%s \n", all)
-		//printCityList(all)
-	}
-*/
-
-	//fetcher.Fetch("https://blog.csdn.net/cui_yonghua/article/details/94561107")
 	all, _ := fetcher.Fetch("http://www.zhenai.com/zhenghun")
-	printCityList(all)
+	cityUrls:= printCityList(all)
+	fmt.Println(cityUrls[1])
+
 }
 
-func printCityList(contents [] byte){
+func printCityList(contents [] byte) []string{
 	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"`)
 	match := re.FindAllSubmatch(contents, -1)
 
+	cityUrls := make([]string, len(match))
+
 	for i:=0; i<len(match); i++{
-		fmt.Println(string(match[i][1]))
+		cityUrls[i] = string(match[i][1])
 	}
+
+	return cityUrls
 }
