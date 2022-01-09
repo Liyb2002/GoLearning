1a468efd8a13a51d28a962acf355cca142acabe8
Liyb2002 <55519796+Liyb2002@users.noreply.github.com>
2022-01-09 13:27:23 -0500

    parse

--- crawler/fetcher/fetcher.go
+++ crawler/fetcher/fetcher.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"io/ioutil"
 )
-
+/*
 func Fetch(url string) ([]byte, error){
+
+
 	resp, err := http.Get(url)
 
 	if err !=nil{
@@ -17,9 +19,30 @@ func Fetch(url string) ([]byte, error){
 	if resp.StatusCode != http.StatusOK{
 		return nil, fmt.Errorf("wrong status code:%d", resp.StatusCode)
 	}
-
 		return ioutil.ReadAll(resp.Body)
 
+}
+*/
+
+func Fetch(url string) ([]byte, error){
+
+	resp, err := http.Get(url)
+	if err !=nil{
+		panic(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK{
+		all, err := ioutil.ReadAll(resp.Body)
+		if err!=nil{
+			panic(err)
+		}
+		//fmt.Printf("%s \n", all)
+		return all, err
+}
+
+return nil, nil
 }
 
 func TestPrint() string{
--- crawler/main.go
+++ crawler/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"regexp"
+	//"net/http"
+//	"io/ioutil"
 	"crawler/fetcher"
 )
 
 func main(){
-	fetcher.TestPrint()
+	//fetcher.TestPrint()
 	/*
 	resp, err := http.Get("http://www.zhenai.com/zhenghun")
 
@@ -21,15 +23,21 @@ func main(){
 		if err!=nil{
 			panic(err)
 		}
-		//fmt.Printf("%s \n", all)
-		printCityList(all)
+		fmt.Printf("%s \n", all)
+		//printCityList(all)
 	}
 */
 
+	//fetcher.Fetch("https://blog.csdn.net/cui_yonghua/article/details/94561107")
+	all, _ := fetcher.Fetch("http://www.zhenai.com/zhenghun")
+	printCityList(all)
 }
 
 func printCityList(contents [] byte){
 	re := regexp.MustCompile(`<a href="(http://www.zhenai.com/zhenghun/[0-9a-z]+)"`)
 	match := re.FindAllSubmatch(contents, -1)
-	fmt.Println(string(match[1][1]))
+
+	for i:=0; i<len(match); i++{
+		fmt.Println(string(match[i][1]))
+	}
 }
