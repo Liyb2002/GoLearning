fcfa000a41e6c79fde60825ab487da50ba377175
Liyb2002 <55519796+Liyb2002@users.noreply.github.com>
2022-01-10 14:16:47 -0500

    getByID

--- getStart/Web/web-service-gin/main.go
+++ getStart/Web/web-service-gin/main.go
@@ -20,9 +20,24 @@ var albums = []album{
     {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
+
+//Get
 func getAlbums(c *gin.Context) {
     c.IndentedJSON(http.StatusOK, albums)
 }
+
+func getAlbumByID(c *gin.Context) {
+    id := c.Param("id")
+
+    for _, a := range albums {
+        if a.ID == id {
+            c.IndentedJSON(http.StatusOK, a)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 
 func postAlbums(c *gin.Context) {
     var newAlbum album
@@ -41,7 +56,9 @@ func postAlbums(c *gin.Context) {
 func main() {
     router := gin.Default()
     router.GET("/albums", getAlbums)
+    router.GET("/albums/:id", getAlbumByID)
     router.POST("/albums", postAlbums)
+    
 
     router.Run("localhost:8080")
 }
